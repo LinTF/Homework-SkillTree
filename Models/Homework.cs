0ed947d48677f43480f5c46c9e986a7adc9262f3
Iris Lin <kc156832@hotmail.com>
2021-08-28 23:44:02 +0800

    完成上方填寫區html helper強行別控制項

---
+++ Models/Homework.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Homework_SkillTree.Models
+{
+    /// <summary>
+    /// 類別
+    /// </summary>
+    public enum MoneyType
+    {
+        支出,
+        收入
+    }
+
+    public class Homework
+    {
+        public MoneyType HomeMoneyType { get; set; }
+        public decimal Price { get; set; }
+        public DateTime Date { get; set; }
+        public string memo { get; set; }
+    }
+}
